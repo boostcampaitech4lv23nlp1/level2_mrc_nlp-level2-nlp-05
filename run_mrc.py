dd120276bc90a3a8b7364b5438fb92c76dfea14e
jstep750 <jstep0000@gmail.com>
2022-12-22 20:32:40 +0000

    add eval & predict in run_mrc

--- run_mrc.py
+++ run_mrc.py
@@ -2,10 +2,20 @@ import logging
 import os
 import sys
 from datetime import datetime
-from typing import NoReturn
+from typing import Callable, Dict, List, NoReturn, Tuple
 
+import numpy as np
 from arguments import DataTrainingArguments, ModelArguments
-from datasets import DatasetDict, load_from_disk, load_metric
+from datasets import (
+    Dataset,
+    DatasetDict,
+    Features,
+    Sequence,
+    Value,
+    load_from_disk,
+    load_metric,
+)
+from retrieval import SparseRetrieval
 from trainer.trainer_qa import QuestionAnsweringTrainer
 from transformers import (
     AutoConfig,
@@ -19,6 +29,7 @@ from transformers import (
 )
 from utils.utils_qa import check_no_error, postprocess_qa_predictions
 from data_loaders.data_loader import load_train_dataset, load_eval_dataset
+from run_sparse_retrieval import run_sparse_retrieval
 
 logger = logging.getLogger(__name__)
 
@@ -47,7 +58,13 @@ def run_mrc(
     )
 
     # Post-processing:
-    def post_processing_function(examples, features, predictions, training_args):
+    def post_processing_function(
+            examples,
+            features,
+            predictions: Tuple[np.ndarray, np.ndarray],
+            training_args: TrainingArguments,
+        ) -> EvalPrediction:
+
         # Post-processing: start logits과 end logits을 original context의 정답과 match시킵니다.
         predictions = postprocess_qa_predictions(
             examples=examples,
@@ -63,6 +80,7 @@ def run_mrc(
         if training_args.do_predict:
             return formatted_predictions
 
+        answer_column_name = "answers" if "answers" in datasets["validation"].column_names else datasets["validation"].column_names[2]
         references = [
             {"id": ex["id"], "answers": ex[answer_column_name]}
             for ex in datasets["validation"]
@@ -75,7 +93,8 @@ def run_mrc(
 
     def compute_metrics(p: EvalPrediction):
         return metric.compute(predictions=p.predictions, references=p.label_ids)
-
+    
+    print("init trainer...")
     # Trainer 초기화
     trainer = QuestionAnsweringTrainer(
         model=model,
@@ -120,6 +139,31 @@ def run_mrc(
             os.path.join(training_args.output_dir, "trainer_state.json")
         )
 
+        # True일 경우 : run passage retrieval
+        if data_args.eval_retrieval:
+            # Evaluate
+            logger.info("*** Evaluate ***")
+            metrics = trainer.evaluate(eval_dataset)
+            metrics["eval_samples"] = len(eval_dataset)
+            trainer.log_metrics("eval", metrics)
+            trainer.save_metrics("eval", metrics)
+
+            test_datasets = load_from_disk('./dataset/test_dataset/')
+            predict_datasets = run_sparse_retrieval(
+                tokenizer.tokenize, test_datasets, training_args, data_args,
+            )
+            predict_dataset = load_eval_dataset(predict_datasets, max_seq_length, data_args, tokenizer)
+
+            # Predict
+            logger.info("*** Predict ***")
+            predictions = trainer.predict(
+                test_dataset=predict_dataset, test_examples=predict_datasets["validation"]
+            )
+            # predictions.json 은 postprocess_qa_predictions() 호출시 이미 저장됩니다.
+            print(
+                "No metric can be presented because there is no correct answer given. Job done!"
+            )
+
     # Evaluation
     if training_args.do_eval:
         logger.info("*** Evaluate ***")
@@ -130,3 +174,14 @@ def run_mrc(
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
 
+    #### eval dataset & eval example - predictions.json 생성됨
+    if training_args.do_predict:
+        logger.info("*** Predict ***")
+        predictions = trainer.predict(
+            test_dataset=eval_dataset, test_examples=datasets["validation"]
+        )
+
+        # predictions.json 은 postprocess_qa_predictions() 호출시 이미 저장됩니다.
+        print(
+            "No metric can be presented because there is no correct answer given. Job done!"
+        )
