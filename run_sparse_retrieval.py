4d63eb3e6ac9bfe11de7046fbcd19118ddd70f72
jstep750 <jstep0000@gmail.com>
2022-12-22 20:33:26 +0000

    resolve run_sparse_retrieval error

--- run_sparse_retrieval.py
+++ run_sparse_retrieval.py
@@ -32,7 +32,7 @@ def run_sparse_retrieval(
         datasets: DatasetDict,
         training_args: TrainingArguments,
         data_args: DataTrainingArguments,
-        data_path: str = "../data",
+        data_path: str = "./dataset",
         context_path: str = "wikipedia_documents.json",
     ) -> DatasetDict:
 
@@ -51,32 +51,13 @@ def run_sparse_retrieval(
         df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
 
     # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
-    if training_args.do_predict:
-        f = Features(
-            {
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
+    f = Features(
+        {
+            "context": Value(dtype="string", id=None),
+            "id": Value(dtype="string", id=None),
+            "question": Value(dtype="string", id=None),
+        }
+    )
+    predict_datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
 
-    # train data 에 대해선 정답이 존재하므로 id question context answer 로 데이터셋이 구성됩니다.
-    elif training_args.do_eval:
-        f = Features(
-            {
-                "answers": Sequence(
-                    feature={
-                        "text": Value(dtype="string", id=None),
-                        "answer_start": Value(dtype="int32", id=None),
-                    },
-                    length=-1,
-                    id=None,
-                ),
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-    datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
-    return datasets
-
+    return predict_datasets
